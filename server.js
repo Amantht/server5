28c57fe090d9fb2480260579455128474c01f3cb
Aman <aman.tht@gmail.com>
2023-03-02 08:27:54 +0530

    registration and ex5 data

---
+++ server.js
@@ -0,0 +1,157 @@
+// /////registration form data 
+// const express = require('express')
+// const cors = require('cors')
+
+// const app = express()
+// app.use(express.json())
+// app.use(cors())
+
+// const { MongoClient } = require('mongodb')
+
+// const uri = "mongodb+srv://admin:admin@cluster0.aqmkc3t.mongodb.net/?retryWrites=true&w=majority";
+
+// const client = new MongoClient(uri);
+// client.connect();
+
+// // // registration form data
+// // const db = client.db("s16");
+// // const col = db.collection("user");
+
+// var db = client.db("S_16");
+// // var db1=client.db("In-Sem");
+// var col = db.collection("Registration");
+// // var col1=db1.collection("Exam");
+
+// ///registration form data
+// app.post('/insert',(request,response) => {
+//   response.send(request.body);
+//   console.log(request.body)
+//   col.insertOne(request.body)
+//   console.log("Documents Inserted");
+
+// })
+// app.get('/',(request,response) => {
+//   response.send('This is a Server')
+// })
+// app.get('/check', (request,response)=> {
+
+//   async function run () {
+//     try {
+//       console.log(request.query.un);
+//       const result = await col.findOne({email:request.query.un})
+//       if (result != null) {
+//         console.log(result.email);
+//         if (result.password === request.query.pw) {
+//           response.send("pass");
+//         }
+//         else {
+//           response.send("fail");
+//         }
+//       }
+//       else {
+//         response.send("fail");
+//       }
+//     }
+//     finally {
+    
+//     }
+//   }
+//   run().catch(console.dir);
+// })
+
+// app.listen(8081)
+// //localhost:8081
+// console.log("server started")
+
+
+
+
+
+
+
+//////exp5 //////
+
+
+// run only one at a time
+
+
+
+
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+const { MongoClient } = require('mongodb')
+
+const uri = "mongodb+srv://admin:admin@cluster0.aqmkc3t.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri);
+client.connect();
+var db = client.db("In-Sem");
+// var col = client.db("Exam");
+
+// register into mongoDB
+app.post('/insert_stu',(req,res)=>{
+  col =db.collection("Exam")
+  console.log(req.body)
+  col.insertOne(req.body)
+  res.send(req.body)
+
+})
+app.get('/show_one_stu',(req,res)=>{
+  async function find(){
+    try{
+      const result=await col.findOne()
+      console.log(result)
+      if(result==null)
+      {
+        res.send({"Data_retrieval":"Fail"})
+      }else{
+          res.send(result);
+        }
+      }
+      finally{}
+    }
+    find().catch(console.dir)
+  })
+
+  app.get('/show_all',(req,res)=>{
+    async function find(){
+      try{
+        const result=await col.find().toArray([])
+        console.log(result)
+        if(result==null)
+        {
+          res.send({"Data_retrieval":"Fail"})
+        }else{
+            res.send(result);
+          }
+        }
+        finally{}
+      }
+      find().catch(console.dir)
+    })
+
+    app.get('/show_all_gt1',(req,res)=>{
+      async function find(){
+        try{
+          const result=await col.find({stu_id:{$gte:1},$or:[{stu_dept:"CSE"},{stu_dept:"ECE"}]}).toArray()
+          console.log(result)
+          if(result==null)
+          {
+            res.send({"Data_retrieval":"Fail"})
+          }else{
+              res.send(result);
+            }
+          }
+          finally{}
+        }
+        find().catch(console.dir)
+      })
+
+  
+app.listen(8082)
+//localhost:8081
