9a97ea2c49d432103d339cf6698b427265f9cf7a
Aman <aman.tht@gmail.com>
2023-04-05 11:59:07 +0530

    minor changes

--- server.js
+++ server.js
@@ -91,11 +91,10 @@ const uri = "mongodb+srv://admin:admin@cluster0.aqmkc3t.mongodb.net/?retryWrites
 const client = new MongoClient(uri);
 client.connect();
 var db = client.db("In-Sem");
-// var col = client.db("Exam");
+var col = db.collection("Exam");
 
 // register into mongoDB
 app.post('/insert_stu',(req,res)=>{
-  col =db.collection("Exam")
   console.log(req.body)
   col.insertOne(req.body)
   res.send(req.body)
@@ -154,18 +153,16 @@ app.get('/show_one_stu',(req,res)=>{
 
       /////exp 8
       app.patch('/update_stu',(req,res)=>{
-        col=db.collection("Exam")
          const doc={
             $set:{stu_dept:"BT",stu_address:"Vaddeswaram"
             }
          }
-         col.updateOne({stu_id:2100032607},doc)
+         col.updateOne({stu_id:2100032572},doc)
          res.send("Updated Succesfully")
         })
           //inserting document
 
         app.post('/insert_student',(req,res)=>{
-          col =db.collection("Exam")
             const doc={stu_name:"Aman",stu_dept:"CSE-H",stu_address:"KLU-Hostel" }
           
          col.insertOne(doc)
@@ -173,7 +170,6 @@ app.get('/show_one_stu',(req,res)=>{
           })
             /// 
           app.delete('/delete_student',(req,res)=>{
-            col=db.collection("Exam")
             //  const doc={
             //     $set:{stu_dept:"BT",stu_address:"Vaddeswaram"
             //     }
@@ -182,4 +178,5 @@ app.get('/show_one_stu',(req,res)=>{
              res.send("Deleted Succesfully")
             })
 app.listen(8082)
+console.log("Server Started")
 //localhost:8081
